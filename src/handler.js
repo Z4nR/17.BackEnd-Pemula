89ef5c71a83b06f6edcbd58c708201cb25e43e35
Zulham Ari <ari.comunity1@gmail.com>
2021-10-09 00:43:00 +0700

    finishing program

--- src/handler.js
+++ src/handler.js
@@ -2,17 +2,12 @@ const { nanoid } = require('nanoid')
 const books = require('./books')
 
 const addBookHandler = (request, h) => {
-  const { name, year, author, summary, publisher, pageCount, readPage } = request.payload
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload
 
   const id = nanoid(16)
   const insertedAt = new Date().toISOString()
   const updatedAt = insertedAt
   const finished = readPage === pageCount
-  let reading = !finished
-
-  if (readPage === 0) {
-    reading = false
-  }
 
   const newBook = {
     id, name, year, author, summary, publisher, pageCount, readPage, finished, reading, insertedAt, updatedAt
